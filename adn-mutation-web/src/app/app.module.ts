b714a65ee68a19d280fe5a25e2318d09cdb93041
Aylin Rios <ariosm.developer@gmail.com>
2023-05-16 10:41:00 -0600

    frontend added

--- adn-mutation-web/src/app/app.module.ts
+++ adn-mutation-web/src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AnalyzeHumanComponent } from './analyze-human/analyze-human.component';
-import { WithMutationComponent } from './with-mutation/with-mutation.component';
-import { WithoutMutationComponent } from './without-mutation/without-mutation.component';
-import { LoginComponent } from './login/login.component';
 import { LoginService } from './login/service/login.service';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,7 +9,6 @@ import { LoginModule } from './login/login.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { HomeComponent } from './home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeModule } from './home/home.module';
 import { HomeService } from './home/service/home.service';
@@ -22,9 +17,6 @@ import { GenericService } from './shared/generic.service';
 @NgModule({
   declarations: [
     AppComponent,
-    AnalyzeHumanComponent,
-    WithMutationComponent,
-    WithoutMutationComponent,
   ],
   imports: [
     BrowserModule,
